664d3374b47503fd7f65b8b1358b0934790f1268
iwenking <201468291@qq.com>
2020-03-19 22:02:30 +0800

    顶部导航条

--- src/App.vue
+++ src/App.vue
@@ -14,22 +14,11 @@ export default {
     }
   },
   mounted(){
-    this.axios.get('/user/login').then((res)=>{
-      // this.res = res
-      console.log(res);
-      
-    })
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import './assets/scss/reset.scss'
 </style>
