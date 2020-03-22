6095bf1e4b2a93a80ba4eb184c5bcff851fceee4
iwenking <201468291@qq.com>
2020-03-22 15:28:38 +0800

    底部组件

--- src/components/NavFooter.vue
+++ src/components/NavFooter.vue
@@ -1,5 +1,17 @@
 <template>
-  <div></div>
+  <div class="footer">
+    <div class="footer-logo">
+      <img src="/imgs/logo-footer.png" alt="">
+      <p>小米商城</p>
+    </div>
+    <div class="footer-link">
+      <a href="javasript:;">小米商城主页</a><span>|</span>
+      <a href="javasript:;">小米商城主页</a><span>|</span>
+      <a href="javasript:;">小米商城主页</a><span>|</span>
+      <a href="javasript:;">小米商城主页</a><span>|</span>
+      <a href="javasript:;">小米商城主页</a>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +20,34 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+.footer{
+  height: 234px;
+  border-top:4px solid #ff7301;
+  background-color: #333;
+  color: #999;
+  font-size: 16px;
+  text-align: center;
+  .footer-logo{
+    margin-top: 46px;
+    margin-bottom: 31px;
+    img{
+      width: 53px;
+      height: 36px;
+      margin-bottom: 13px;
+    }
+  }
+  .footer-link{
+    a{
+      color: #999;
+      display: inline-block;
+    }
+    span{
+      margin: 0 10px;
+
+    }
+  }
+}
+
+
 </style>
